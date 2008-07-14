c6128d4d02595e254060087b3555c0a628446709
Nicolas Chauvat <nicolas.chauvat@logilab.fr>
2008-07-14 20:57:35 +0200

    improve doc for better epydoc generation.

--- __init__.py
+++ __init__.py
@@ -1,24 +1,20 @@
-# (c) 2000-2003 LOGILAB S.A. (Paris, FRANCE).
-# http://www.logilab.fr/ -- mailto:contact@logilab.fr
-#
-# This program is free software; you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free Software
-# Foundation; either version 2 of the License, or (at your option) any later
-# version.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along with
-# this program; if not, write to the Free Software Foundation, Inc.,
-# 59 Temple Place - Suite 330, Boston, MA  02111-1307
-# USA.
 """
-logilab.constraint package. Constraint Propagation Solver in Python.
+Constraint Satisfaction Problem (CSP) Solver in Python.
+
+:copyright:
+  2000-2008 `LOGILAB S.A. <http://www.logilab.fr>`_ (Paris, FRANCE),
+  all rights reserved.
+
+:contact:
+  http://www.logilab.org/project/logilab-constraint --
+  mailto:python-projects@logilab.org
+
+:license:
+  `General Public License version 2
+  <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>`_
 """
 
-
+from logilab.constraint.__pkginfo__ import version as __version__
 from logilab.constraint.propagation import Repository, Solver
 from logilab.constraint.distributors import DefaultDistributor
 from logilab.constraint import fd
