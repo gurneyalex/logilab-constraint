b4a2a3eb943f23446568ec56fbf7d4d4dd077467
Nicolas Chauvat <nicolas.chauvat@logilab.fr>
2010-11-12 07:20:42 +0100

    stable is now 0.4.1

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -18,7 +18,7 @@
 # with logilab-constraint. If not, see <http://www.gnu.org/licenses/>.
 
 modname = 'constraint'
-distname = 'constraint'
+distname = modname
 
 numversion = (0, 4, 1)
 version = '.'.join(map(str, numversion))
